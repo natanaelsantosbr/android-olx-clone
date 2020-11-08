1368203a5e82c65d05d4a94c21d3b849d6251e2e
Natanael <natanaelsantosbr@gmail.com>
2020-11-08 00:01:33 -0300

    Validar Permissões de Acesso a Internet, Verificar Status da Internet e Acesso a Galeria

--- app/src/main/java/br/natanael/android/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/br/natanael/android/olx/activity/CadastrarAnuncioActivity.java
@@ -1,7 +1,12 @@
 package br.natanael.android.olx.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,21 +14,32 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
+import com.santalu.maskara.widget.MaskEditText;
 
 import java.text.DecimalFormat;
 import java.util.Locale;
 
 import br.natanael.android.olx.R;
+import br.natanael.android.olx.helper.Permissoes;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
     private EditText editTitulo,editDescricao;
     private CurrencyEditText editValor;
     private Button btnSalvarAnuncio;
+    private MaskEditText editTelefone;
+
+    private String[] permissoes = new String[] {
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        Permissoes.validarPermissoes(permissoes, this, 1);
 
         inicializarComponentes();
     }
@@ -33,6 +49,7 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         editDescricao = findViewById(R.id.editDescricao);
         editValor = findViewById(R.id.editValor);
         btnSalvarAnuncio = findViewById(R.id.btnSalvarAnuncio);
+        editTelefone = findViewById(R.id.editTelefone);
 
         Locale locale = new Locale("pt", "BR");
         editValor.setLocale(locale);
@@ -40,5 +57,33 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     public void salvarAnuncio(View view){
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResultado : grantResults)
+        {
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitas as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
