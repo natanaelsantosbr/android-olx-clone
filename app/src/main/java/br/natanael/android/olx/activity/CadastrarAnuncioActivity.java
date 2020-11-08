7f52de47fd813f86d8c40c52278a08d884348e53
Natanael <natanaelsantosbr@gmail.com>
2020-11-08 02:33:51 -0300

    Escolher as fotos do anuncio

--- app/src/main/java/br/natanael/android/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/br/natanael/android/olx/activity/CadastrarAnuncioActivity.java
@@ -1,32 +1,42 @@
 package br.natanael.android.olx.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.santalu.maskara.widget.MaskEditText;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import br.natanael.android.olx.R;
 import br.natanael.android.olx.helper.Permissoes;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editTitulo,editDescricao;
     private CurrencyEditText editValor;
     private Button btnSalvarAnuncio;
     private MaskEditText editTelefone;
+    private ImageView imageCadastro1, imageCadastro2,imageCadastro3;
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     private String[] permissoes = new String[] {
             Manifest.permission.INTERNET,
@@ -50,6 +60,13 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         editValor = findViewById(R.id.editValor);
         btnSalvarAnuncio = findViewById(R.id.btnSalvarAnuncio);
         editTelefone = findViewById(R.id.editTelefone);
+        imageCadastro1 = findViewById(R.id.imageCadastro1);
+        imageCadastro2 = findViewById(R.id.imageCadastro2);
+        imageCadastro3 = findViewById(R.id.imageCadastro3);
+
+        imageCadastro1.setOnClickListener(this);
+        imageCadastro2.setOnClickListener(this);
+        imageCadastro3.setOnClickListener(this);
 
         Locale locale = new Locale("pt", "BR");
         editValor.setLocale(locale);
@@ -85,5 +102,47 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.imageCadastro1:
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2:
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3:
+                escolherImagem(3);
+                break;
+        }
+    }
+
+    public void escolherImagem(int requestCode) {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK)
+        {
+            //Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configurar imagem no ImageView
+            if(requestCode == 1)
+                imageCadastro1.setImageURI(imagemSelecionada);
+            else if(requestCode == 2)
+                imageCadastro2.setImageURI(imagemSelecionada);
+            else if(requestCode == 3)
+                imageCadastro3.setImageURI(imagemSelecionada);
+
+            listaFotosRecuperadas.add(caminhoImagem);
+        }
     }
 }
