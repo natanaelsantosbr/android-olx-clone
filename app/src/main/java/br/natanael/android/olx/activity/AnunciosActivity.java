59d960eea68b8279fbb5d425d523e4d730e74bc4
Natanael <natanaelsantosbr@gmail.com>
2020-11-08 22:44:57 -0300

    Aplicar Filtros por Categoria

--- app/src/main/java/br/natanael/android/olx/activity/AnunciosActivity.java
+++ app/src/main/java/br/natanael/android/olx/activity/AnunciosActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -43,6 +44,8 @@ public class AnunciosActivity extends AppCompatActivity {
     private DatabaseReference anunciosPublicosRef;
     private AlertDialog dialog;
     private String filtroEstado = "";
+    private String filtroCategoria = "";
+    private  boolean filtrandoPorEstado = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -162,6 +165,7 @@ public class AnunciosActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 filtroEstado = spinnerEstado.getSelectedItem().toString();
+                filtrandoPorEstado = true;
                 recuperarAnunciosPorEstado();
 
             }
@@ -201,6 +205,87 @@ public class AnunciosActivity extends AppCompatActivity {
                         Anuncio anuncio = anuncios.getValue(Anuncio.class);
                         listaAnuncios.add(anuncio);
                     }
+                }
+
+                Collections.reverse(listaAnuncios);
+                adapterAnuncios.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void filtrarPorCategoria(View view)
+    {
+        if(!filtrandoPorEstado){
+            Toast.makeText(this, "Escolha primeiro um Estado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        AlertDialog.Builder dialogCategoria = new AlertDialog.Builder(this);
+        dialogCategoria.setTitle("Selecione a Categoria desejado");
+
+        //configurar spinner
+        View viewSpinner = getLayoutInflater().inflate(R.layout.dialog_spinner, null);
+
+        final Spinner spinnerCategoria = viewSpinner.findViewById(R.id.spinnerFilltro);
+        String[] categorias = getResources().getStringArray(R.array.categoria);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, categorias);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategoria.setAdapter(adapter);
+
+        dialogCategoria.setView(viewSpinner);
+
+
+
+        dialogCategoria.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                filtroCategoria = spinnerCategoria.getSelectedItem().toString();
+                recuperarAnunciosPorCategoria();
+
+            }
+        });
+
+        dialogCategoria.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog dialog = dialogCategoria.create();
+        dialog.show();
+    }
+
+    private void recuperarAnunciosPorCategoria() {
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Carregando...")
+                .setCancelable(true)
+                .build();
+
+        dialog.show();
+
+        anunciosPublicosRef = ConfiguracaoFirebase.getFirebase()
+                .child("anuncios")
+                .child(filtroEstado)
+                .child(filtroCategoria);
+
+
+        anunciosPublicosRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listaAnuncios.clear();
+                for (DataSnapshot anuncios : snapshot.getChildren()){
+                    Anuncio anuncio = anuncios.getValue(Anuncio.class);
+                    listaAnuncios.add(anuncio);
                 }
 
                 Collections.reverse(listaAnuncios);
